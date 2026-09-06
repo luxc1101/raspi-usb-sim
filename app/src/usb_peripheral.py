431fe49cd6a8dece1838372743db7fe4aaf8404d
Xiaochuan Lu <xiaochuan_lu@yahoo.com>
2026-09-06 17:42:27 +0200

    fix: improve function removal in enable_the_gadget method for USBPeripheral

--- app/src/usb_peripheral.py
+++ app/src/usb_peripheral.py
@@ -57,10 +57,10 @@ class USBPeripheral(ADevice):
         '''
         subprocess.run('sudo systemctl is-active --quiet getty@ttyGS0.service && sudo systemctl stop getty@ttyGS0.service', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         subprocess.run('sudo bash -c "echo '' > /sys/kernel/config/usb_gadget/g1/UDC"', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # disabling the gadget
-        subprocess.run('func="$(ls /sys/kernel/config/usb_gadget/g1/functions/)" && sudo rm /sys/kernel/config/usb_gadget/g1/configs/c.1/$func ', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # remove functions from configuration
+        subprocess.run('func="$(ls /sys/kernel/config/usb_gadget/g1/functions/)"; for f in $func; do sudo rm /sys/kernel/config/usb_gadget/g1/configs/c.1/$f; done', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # remove functions from configuration
         subprocess.run('sudo rmdir /sys/kernel/config/usb_gadget/g1/configs/c.1/strings/0x409', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # remove string dir in the configuration
         subprocess.run('sudo rmdir /sys/kernel/config/usb_gadget/g1/configs/c.1', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # remove the configurations
-        subprocess.run('func="$(ls /sys/kernel/config/usb_gadget/g1/functions/)" && sudo rmdir /sys/kernel/config/usb_gadget/g1/functions/$func', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # remove functions
+        subprocess.run('func="$(ls /sys/kernel/config/usb_gadget/g1/functions/)"; for f in $func; do sudo rmdir /sys/kernel/config/usb_gadget/g1/functions/$f; done', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # remove functions
         subprocess.run('sudo rmdir /sys/kernel/config/usb_gadget/g1/strings/0x409', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # remove string dir in gadget
         subprocess.run('sudo rmdir /sys/kernel/config/usb_gadget/g1', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # finally remove the while gadget
         subprocess.run('sudo pkill umtprd', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
